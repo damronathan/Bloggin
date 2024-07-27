69c406444f70453121686ca3067f6a6b7ef9a813
damronathan <damronnathan14@gmail.com>
2024-07-27 14:35:20 -0700

    Started Project

---
+++ SimpleBlogDB/Tables/BlogPost.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[BlogPost]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Title] NVARCHAR(50) NULL, 
+    [Content] NVARCHAR(MAX) NULL, 
+    [Author] NVARCHAR(50) NULL, 
+    [CreatedAt] DATETIME NULL
+)
